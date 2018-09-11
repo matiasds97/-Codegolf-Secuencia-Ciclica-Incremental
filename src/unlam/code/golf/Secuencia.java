f53cc7e0e09f6db4b4ac5042a17ed818ade52536
Matias De Stefano <matiasdestefano97@gmail.com>
2018-09-11 20:14:19 -0300

    180 Caracteres - Pablo Guayta

--- src/unlam/code/golf/Secuencia.java
+++ src/unlam/code/golf/Secuencia.java
@@ -3,17 +3,17 @@ package unlam.code.golf;
 public class Secuencia {
 	
 	boolean secuenciaCiclica(int[] secuencia) {
-		int[] s = secuencia;
-		int i = 0, j = 1, l = s.length;
-		boolean k = 0>1;
-		
-		while(s[i++] < s[j++]);
-				j = i;
-		
-		while(l > 1) {
-			j = j < l? j: 0;
-			k = (s[i] > s[++j])? 0>1: 1>0;
-		}
-		return k;
+	int[] s = secuencia;
+	int i = 0, p = 0, l = s.length;
+	
+	for(; i < l; i++)
+		p = s[i] < s[p]? i: p;
+	
+	i = p < 1? l - 1 : p - 1;
+	
+	while (p != i)
+		if (s[p] > s[p + 1 == l ? p = 0 : ++p])
+			return 0 > 1;
+	return 1 > 0;
 	}
 }

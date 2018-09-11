743986ccb60c7b3f738e2342025034c5df30d858
Matias De Stefano <matiasdestefano97@gmail.com>
2018-09-11 19:27:34 -0300

    172 Caracteres - No Anda

--- src/unlam/code/golf/Secuencia.java
+++ src/unlam/code/golf/Secuencia.java
@@ -4,23 +4,16 @@ public class Secuencia {
 	
 	boolean secuenciaCiclica(int[] secuencia) {
 		int[] s = secuencia;
-		int i = 0, j = 0, l = s.length;
-		for(; i < l-1; i++) {
-			if(s[i] > s[i+1])
-				j = i+1;
+		int i = 0, j = 1, l = s.length;
+		boolean k = 0>1;
+		
+		while(s[i++] < s[j++]);
+				j = i;
+		
+		while(l > 1) {
+			j = j < l? j: 0;
+			k = (s[i] > s[++j])? 0>1: 1>0;
 		}
-		i = j+1;
-		if(l < 2)
-			return 1>0;
-		while(i != j) {
-			if(j > l-2) {
-				if(s[j] > s[0])
-					return 0>1;
-				j = 0;
-			}
-			if(s[j] > s[++j])
-				return 0>1;
-		}
-		return 1>0;
+		return k;
 	}
 }
